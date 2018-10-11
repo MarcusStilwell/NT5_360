398c1f2ae75a77814d77f21c4966e3a2847dceab
James Yu <jamesyu@Jamess-MacBook-Pro.local>
2018-10-10 21:45:16 -0700

    added main file

---
+++ Proficient_paths/src/Main_Window.java
@@ -0,0 +1,23 @@
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Main_Window extends JFrame {
+	JPanel main_layout = new JPanel();
+
+	public static void main(String[] args) {
+		new Main_Window();
+		 
+	}
+	public Main_Window() {
+		super("Profecient Paths");
+		setSize(400, 300);
+		setResizable(true);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		main_layout.setLayout(new BorderLayout());
+		main_layout.add
+		
+	}
+}
